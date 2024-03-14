fce199d9f3a08c01bd0d1aff5d572f2af9816163
topherwalker <git@christopherwalker.tech>
2024-03-14 16:53:24 +0000

    feat: give helpful error message if access non endpoints

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { Request, Response } from 'express';
 import * as booksController from "../controllers/books_controller";
 
 export const router = express.Router();
@@ -11,3 +12,16 @@ router.put("/books/:bookId", booksController.updateBook);
 
 // Lab 1: task 1 - delete book by id
 router.delete("/books/:bookId", booksController.deleteBook);
+
+router.all("*", (req: Request, res: Response) => {
+    const routes = router.stack.map(layer => {
+        // Check if the layer has a route and a path
+        if (layer.route && layer.route.path) {
+          return {
+            path: layer.route.path,
+            methods: Object.keys(layer.route.methods)
+          };
+        }
+      }).filter(item => item !== undefined);
+    res.status(404).json({error:{type:"no end point here", valid_endpoints: routes}})
+})
